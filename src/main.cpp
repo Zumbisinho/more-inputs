98312e8ba552e21dbe7dd3da6f59cae4937cd6b5
Zumbisinho <seuemail@example.com>
2026-04-08 22:51:39 -0300

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/GJEffectManager.hpp>
+
+// TODO Encode keysbinds via some shit with object encoding (Or just lazy json on text label)
+
+using namespace geode::prelude;
+
+class $modify(MyPlayLayer, GJEffectManager)
+{
+	bool init(){
+		if (!GJEffectManager::init())
+            return false;
+		
+		this->addEventListener(
+			KeybindSettingPressedEventV3(Mod::get(),"test-key"),[this](Keybind const& keybind, bool down, bool repeat, double timestamp)
+			{
+				if (down && !repeat)
+				{
+					auto manager = this; 
+					manager->addCountToItem(3, 32); 
+					log::info("Variável 3 = 43");
+				}
+			}
+		);
+		return true;
+		
+	};
+};
