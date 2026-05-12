48cf69109b9acf7a00f0d0b7f5640e030b3f3ff2
Zumbisinho <seuemail@example.com>
2026-05-11 23:42:46 -0300

    Added more exceptions to the keybinds.cpp

--- src/hooks/keybinds.cpp
+++ src/hooks/keybinds.cpp
@@ -2,18 +2,25 @@
 #include "../utils/pickupManager.hpp"
 #include "Geode/DefaultInclude.hpp"
 #include "Geode/loader/Event.hpp"
+#include "Geode/loader/Log.hpp"
 #include "Geode/utils/Keyboard.hpp"
 #include <Geode/Geode.hpp>
 #include <Geode/binding/GJBaseGameLayer.hpp>
+#include <Geode/binding/LevelEditorLayer.hpp>
 #include <Geode/binding/PlayLayer.hpp>
 #include <Geode/modify/CCKeyboardDispatcher.hpp>
 
 $execute {
     KeyboardInputEvent()
         .listen(+[](const geode::KeyboardInputData &event) {
-            if (!GJBaseGameLayer::get()) {
+            auto editorLayer = LevelEditorLayer::get();
+            if (editorLayer)
+                if(editorLayer->m_playbackMode != PlaybackMode::Playing)
+                    return geode::ListenerResult::Propagate;
+            if (!GJBaseGameLayer::get() ) {
                 return geode::ListenerResult::Propagate;
             };
+            log::info("Ta otimo pra fazer isso");
             auto layer = !GJBaseGameLayer::get();
             int keyAsInt = static_cast<int>(event.key);
             auto actions = KeybindCache::keyToActionIds.find(keyAsInt);
