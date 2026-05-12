d49cd4ee632ed5d80ee0a45cce8fe01fe4159fb7
Zumbisinho <seuemail@example.com>
2026-05-11 23:00:49 -0300

    Refactored the keybinds.cpp to a much stable version (Thanks iAndyHD3)

--- src/hooks/keybinds.cpp
+++ src/hooks/keybinds.cpp
@@ -1,87 +1,33 @@
 #include "../utils/keybindsCache.hpp"
-#include "../utils/keycodeToString.hpp"
 #include "../utils/pickupManager.hpp"
-#include "Geode/cocos/robtop/keyboard_dispatcher/CCKeyboardDelegate.h"
-#include "Geode/loader/Log.hpp"
+#include "Geode/DefaultInclude.hpp"
+#include "Geode/loader/Event.hpp"
+#include "Geode/utils/Keyboard.hpp"
 #include <Geode/Geode.hpp>
+#include <Geode/binding/GJBaseGameLayer.hpp>
 #include <Geode/binding/PlayLayer.hpp>
 #include <Geode/modify/CCKeyboardDispatcher.hpp>
 
-class $modify(MyKeyboard, CCKeyboardDispatcher) {
-    bool isSafeKey(enumKeyCodes key) {
- 
-        switch (key) {
-        case KEY_LeftWindowsKey:
-        case KEY_RightWindowsKey:
-
-            return false;
-        default:
-            return true;
-        }
-    }
-    bool
-    dispatchKeyboardMSG(enumKeyCodes key, bool down, bool repeat, double dt) {
-        static bool s_processing = false;
-       
-        if (!isSafeKey(key))
-            return false;
-
-        if (s_processing || repeat || KeybindCache::keybinds.empty())
-            return false;
-
-        s_processing = true;
-
-        auto playLayer = PlayLayer::get();
-        auto editorLayer = LevelEditorLayer::get();
-
-        if (playLayer || editorLayer) {
-            if (playLayer) {
-                if (!playLayer->m_player1 || playLayer->m_isPaused ||
-                    playLayer->m_playerDied) {
-                    s_processing = false;
-                    return CCKeyboardDispatcher::dispatchKeyboardMSG(
-                        key, down, repeat, dt
-                    );
+$execute {
+    KeyboardInputEvent()
+        .listen(+[](const geode::KeyboardInputData &event) {
+            if (!GJBaseGameLayer::get()) {
+                return geode::ListenerResult::Propagate;
+            };
+            auto layer = !GJBaseGameLayer::get();
+            int keyAsInt = static_cast<int>(event.key);
+            auto actions = KeybindCache::keyToActionIds.find(keyAsInt);
+            if (KeybindCache::keybinds.contains(keyAsInt)) {
+                for (const int actionID : actions->second) {
+                    Loader::get()->queueInMainThread([=] {
+                        pickupManager::changePickupId(
+                            actionID, event.action == KeyboardInputData::Action::Press ? KeybindCache::value : 0
+                        );
+                    });
                 }
             }
-            if (editorLayer) {
-                if (editorLayer->m_playbackMode == PlaybackMode::Not) {
-                    geode::log::info(
-                        "I dont wanna on the editor, get out of my way!"
-                    );
-                    s_processing = false;
-                    return CCKeyboardDispatcher::dispatchKeyboardMSG(
-                        key, down, repeat, dt
-                    );
-                }
-            }
-            int keyInt = static_cast<int>(key);
+            return geode::ListenerResult::Propagate;
+        })
+        .leak();
+}
 
-            if (KeybindCache::keybinds.contains(keyInt)) {
-                auto it = KeybindCache::keyToActionIds.find(keyInt);
-                if (it != KeybindCache::keyToActionIds.end()) {
-                    if (down) // self-explaning
-                        for (const int actionID : it->second) {
-                            Loader::get()->queueInMainThread([=] {
-                                pickupManager::changePickupId(
-                                    actionID, KeybindCache::value
-                                );
-                            });
-                        };
-                    if (!down) // On upkey event
-                        for (const int actionID : it->second) {
-                            Loader::get()->queueInMainThread([=] {
-                                pickupManager::changePickupId(actionID, 0);
-                            });
-                        };
-                }
-            }
-        }
-
-        auto result =
-            CCKeyboardDispatcher::dispatchKeyboardMSG(key, down, repeat, dt);
-
-        s_processing = false;
-        return result;
-    }
-};
